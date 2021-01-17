7450e9fbdbf9656de4ccf5ae2327e4d54a9f3bab
Tristan <mytic12@hotmail.fr>
2021-01-17 21:21:07 +0100

    :wrench: modif smoke test

--- test_heroku.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-HEROKU_APP_NAME="calm-crag-04725"
-HTTPCODE=`curl -s -o /dev/null -w "%{http_code}" https://$HEROKU_APP_NAME.herokuapp.com/api/articles`
-if [ "$HTTPCODE" -ne 200 ];then
-    echo "heroku app not responding, failing deploy"
-    exit 1
-fi
