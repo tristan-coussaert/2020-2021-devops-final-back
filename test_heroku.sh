da4272d7bd69d2a9de357e999505dadfba532df1
Tristan <mytic12@hotmail.fr>
2021-01-17 19:28:04 +0100

    :wrench: update Procfile/test_heroku/config.yml

--- test_heroku.sh
+++ test_heroku.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 HEROKU_APP_NAME="calm-crag-04725"
-HTTPCODE=`curl -s -o /dev/null -w "%{http_code}" https://$HEROKU_APP_NAME.herokuapp.com/`
+HTTPCODE=`curl -s -o /dev/null -w "%{http_code}" https://$HEROKU_APP_NAME.herokuapp.com/api/articles`
 if [ "$HTTPCODE" -ne 200 ];then
     echo "heroku app not responding, failing deploy"
     exit 1
