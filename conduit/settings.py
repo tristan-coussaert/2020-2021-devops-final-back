520a95e69116521d77e19c83854ae3c73adf36ba
Tristan <mytic12@hotmail.fr>
2021-01-17 20:20:37 +0100

    :wrench: add http url

--- conduit/settings.py
+++ conduit/settings.py
@@ -28,6 +28,7 @@ class Config(object):
         'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
         'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
         'https://upbeat-keller-815ecc.netlify.app',
+        'http://upbeat-keller-815ecc.netlify.app',
         os.environ.get('FRONT_PROD_URL', ''),
     ]
     JWT_HEADER_TYPE = 'Token'
