b6006a0fdb0fba48ae04557b9612809c8d166923
Tristan <mytic12@hotmail.fr>
2021-01-17 18:03:21 +0100

    :wrench: modif front url

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,6 +25,10 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
+        'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
+        'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
+        'https://upbeat-keller-815ecc.netlify.app',
+        os.environ.get('FRONT_PROD_URL', ''),
     ]
     JWT_HEADER_TYPE = 'Token'
 
